f3a79a6b8307f9f4f1d06ec5510e8d6ce1faf0d0
melissafalcao <melissacorreafalcao@gmail.com>
2024-03-27 16:11:30 -0300

    Create user service

---
+++ src/user/user.service.ts
@@ -0,0 +1,57 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  async createUser(createUserDto: CreateUserDto): Promise<User> {
+    const newUser = this.usersRepository.create(createUserDto);
+    return await this.usersRepository.save(newUser);
+  }
+
+  async getUsers(): Promise<User[]> {
+    return await this.usersRepository.find();
+  }
+
+  async getByEmail(email: string): Promise<User> {
+    const user = await this.usersRepository.findOne({
+      where: { email: email },
+    });
+    if (!user) {
+      throw new NotFoundException(`User with email '${email}' not found`);
+    }
+    return user;
+  }
+
+  async getUserById(id: number): Promise<User> {
+    const user = await this.usersRepository.findOne({ where: { id: id } });
+    if (!user) {
+      throw new NotFoundException(`User with ID '${id}' not found`);
+    }
+    return user;
+  }
+
+  async updateUser(id: number, updateUserDto: UpdateUserDto): Promise<User> {
+    const user = await this.getUserById(id);
+    this.usersRepository.merge(user, updateUserDto);
+    return await this.usersRepository.save(user);
+  }
+
+  async deleteUser(id: number): Promise<User> {
+    const user = await this.getUserById(id);
+    return await this.usersRepository.remove(user);
+  }
+
+  async comparePassword(user: User, password: string): Promise<boolean> {
+    return bcrypt.compare(password, user.senha);
+  }
+}
