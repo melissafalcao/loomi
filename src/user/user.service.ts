dcc47bf2986a1f119534bb43e20171262e0b45af
melissafalcao <melissacorreafalcao@gmail.com>
2024-03-27 16:16:47 -0300

    create user entity and update dependencies

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,8 +2,8 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
-import { CreateUserDto } from './dtos/create-user.dto';
-import { UpdateUserDto } from './dtos/update-user.dto';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 import * as bcrypt from 'bcrypt';
 
 @Injectable()
