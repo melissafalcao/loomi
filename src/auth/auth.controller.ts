fb4c4752205982a0043cb6e7aee04b3f9ea04a2f
melissafalcao <melissacorreafalcao@gmail.com>
2024-03-27 16:27:20 -0300

    Create auth controller and service

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,27 @@
+import {
+  Controller,
+  Post,
+  Body,
+  ValidationPipe,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('login')
+  async login(@Body(ValidationPipe) loginDto: LoginDto) {
+    const { email, password } = loginDto;
+    const user = await this.authService.validateUser(email, password);
+
+    if (!user) {
+      throw new UnauthorizedException('Credenciais inválidas');
+    }
+
+    const token = await this.authService.generateToken(user.id, user.type);
+    return { token };
+  }
+}
