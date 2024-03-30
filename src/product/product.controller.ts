f7a7cf4f75e8b6607a702d1fc95ee8712c492ca7
melissafalcao <melissacorreafalcao@gmail.com>
2024-03-30 20:05:12 -0300

    Create product module, service and controller

---
+++ src/product/product.controller.ts
@@ -0,0 +1,51 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Put,
+  Delete,
+  ParseIntPipe,
+} from '@nestjs/common';
+import { ProductService } from './product.service';
+import { Product } from './product.entity';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+
+@Controller('products')
+export class ProductController {
+  constructor(private readonly productService: ProductService) {}
+
+  @Post('create')
+  async createProduct(
+    @Body() createProductDto: CreateProductDto,
+  ): Promise<Product> {
+    return await this.productService.createProduct(createProductDto);
+  }
+
+  @Get()
+  async getAllProducts(): Promise<Product[]> {
+    return await this.productService.getAllProducts();
+  }
+
+  @Get(':id')
+  async getProductById(
+    @Param('id', ParseIntPipe) id: number,
+  ): Promise<Product> {
+    return await this.productService.getProductById(id);
+  }
+
+  @Put(':id')
+  async updateProduct(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateProductDto: UpdateProductDto,
+  ): Promise<Product> {
+    return await this.productService.updateProduct(id, updateProductDto);
+  }
+
+  @Delete(':id')
+  async deleteProduct(@Param('id', ParseIntPipe) id: number): Promise<void> {
+    await this.productService.deleteProduct(id);
+  }
+}
