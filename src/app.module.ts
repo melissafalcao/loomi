58a696649811ddda7df71370816ac5804847b11c
melissafalcao <melissacorreafalcao@gmail.com>
2024-03-30 12:42:57 -0300

    Update AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,27 +2,31 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { ClientModule } from './client/client.module';
 import { AuthModule } from './auth/auth.module';
-
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user/user.entity';
+import { Client } from './client/client.entity';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      entities: [User],
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '',
+      database: 'loomi-bd',
+      entities: [User, Client],
       synchronize: true,
     }),
     UserModule,
+    ClientModule,
     AuthModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, JwtAuthGuard],
+  exports: [JwtAuthGuard],
 })
 export class AppModule {}
