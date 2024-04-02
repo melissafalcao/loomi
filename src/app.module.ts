3454b7bfdb57ae953e7bfda571af63f2e4451250
melissafalcao <melissacorreafalcao@gmail.com>
2024-04-02 13:07:15 -0300

    Docker config

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,11 +19,11 @@ import { OrderModule } from './order/order.module';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '',
-      database: 'loomi-bd',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       entities: [User, Client, Product, Order, OrderItem],
       synchronize: true,
     }),
